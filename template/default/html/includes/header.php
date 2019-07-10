2f4b4f9c6277a627e5b912b2ee2ab6f6961e3e6c
tee1bn <tee01bn@gmail.com>
2019-07-11 00:08:53 +0100

    Update header.php

--- template/default/html/includes/header.php
+++ template/default/html/includes/header.php
@@ -10,7 +10,7 @@
     <meta name="keywords" content="<?=@$page_keywords;?>">
     <meta name="author" content="<?=$page_author;?>">
     <title><?=@$page_title;?></title>
-    <link rel="apple-touch-icon" href="<?=$asset;?>/images/ico/apple-icon-120.png">
+    <link rel="apple-touch-icon" href="<?=$logo;?>">
     <link rel="shortcut icon" type="image/x-icon" href="<?=$logo;?>">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i%7COpen+Sans:300,300i,400,400i,600,600i,700,700i" rel="stylesheet">
 
@@ -38,6 +38,7 @@
     <!-- BEGIN: Custom CSS-->
     <link rel="stylesheet" type="text/css" href="../../../assets/css/style.css">
     <!-- END: Custom CSS-->
+    <link rel="stylesheet" type="text/css" href="<?=asset;?>/fonts/feather/style.min.css">
 
   </head>
   <!-- END: Head-->
