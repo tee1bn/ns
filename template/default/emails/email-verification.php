f3f3314d45ea5f6a083edf6d8a36586443230bc8
tee1bn <tee01bn@gmail.com>
2019-07-14 16:44:31 +0100

    adjusted further

--- template/default/emails/email-verification.php
+++ template/default/emails/email-verification.php
@@ -11,7 +11,7 @@
       Please confirm your email to continue or copy the email code into the input field.
       <br>
       <br>
-       <a href="<?=domain;?>/register/confirm_email/<?=$data['email_verification_token'];?>/<?=$data['email'];?>" style="color: #ffffff; text-decoration: none; margin: 0px; text-align: center; vertical-align: baseline; padding: 9px 9px; font-size: 15px; line-height: 21px; background-color: #32c89a;">&nbsp; Confirm &nbsp;</a>
+       <a href="<?=domain;?>/register/confirm_email/<?=$data['email'];?>/<?=$data['email_verification_token'];?>" style="color: #ffffff; text-decoration: none; margin: 0px; text-align: center; vertical-align: baseline; padding: 9px 9px; font-size: 15px; line-height: 21px; background-color: #32c89a;">&nbsp; Confirm &nbsp;</a>
        <br>
        <br>
      <!--   Or
