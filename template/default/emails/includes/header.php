8ab39a9d2f96111fb3545e968c5261e98613369d
Taiwo <tee02bn@gmail.com>
2020-04-20 22:13:00 +0100

    Update header.php

--- template/default/emails/includes/header.php
+++ template/default/emails/includes/header.php
@@ -25,7 +25,7 @@
                   <div style='max-width: 95%; margin: 0 auto; padding: 10px; font-size: 14px; font-family: Verdana;'>
                       <br>
                       <center style="text-align: center;">
-                        <img src='<?=domain;?>/<?=$logo;?>' style="height: 50px;"/>
+                        <img src='<?=$logo;?>' style="height: 50px;"/>
                       </center>
                       <br>
                       <div style='background-color: #FFFFFF; padding: 15px; margin: 5px 0 5px 0;'>
